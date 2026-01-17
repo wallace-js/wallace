26801d872f15ff67368fc9a23a281f69bf7488e9
Andrew Buchan <andyhasit@gmail.com>
2026-01-17 16:12:44 +0000

    Added flags to babel config files

--- packages/wallace/babel.config.cjs
+++ packages/wallace/babel.config.cjs
@@ -2,5 +2,17 @@
  * Mainly used for running files through babel directly to see output.
  */
 module.exports = {
-  plugins: [["babel-plugin-wallace", {}], "@babel/plugin-syntax-jsx"],
+  plugins: [
+    [
+      "babel-plugin-wallace",
+      {
+        flags: {
+          useControllers: true,
+          useStubs: true,
+          useMethods: true
+        }
+      }
+    ],
+    "@babel/plugin-syntax-jsx"
+  ]
 };
